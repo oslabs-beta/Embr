b8f294f2f3ddf8ecc755fc07a0148f6267418101
Davidasahi <maple11303@gmail.com>
2023-12-06 19:23:14 -0500

    Basic front end finished

--- primary-app/src/app/components/functionrow.tsx
+++ primary-app/src/app/components/functionrow.tsx
@@ -2,7 +2,19 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Flex, Bold, Card, Title, Text, Metric, BarList } from '@tremor/react';
+
+import { CiTimer } from 'react-icons/ci';
+
+import {
+  Flex,
+  Bold,
+  Card,
+  Title,
+  Text,
+  Metric,
+  BarList,
+  Button,
+} from '@tremor/react';
 ``;
 const data = [
   {
@@ -15,28 +27,21 @@ const data = [
     value: 191,
   },
 ];
+
 const functionrow = () => {
   return (
-    <Flex flexDirection="col">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <div>
+      <Flex flexDirection="col">
         <Card>
-          <Flex>
-            <Card style={{ minWidth: '350px' }}>
-              <Title>Inovcations</Title>
-            </Card>
-            {/*<Card>
-          <Flex flexDirection="row">
-            <Badge color={'blue'}>.</Badge>
-            <Badge color={'orange'}>.</Badge>
-            <Badge color={'orange'}>.</Badge>
-            <Badge color={'orange'}>.</Badge>
-            <Badge color={'orange'}>.</Badge>
-            <Badge color={'orange'}>.</Badge>
-            <Badge color={'orange'}>.</Badge>
-          </Flex>
-        </Card>*/}
-            <Card style={{ minWidth: '260px' }} className="max-w-lg">
-              <Flex className="mt-4">
+          <Flex className="max-w: 300, max-h: 300">
+            <Flex>
+              <Title className="mx-5" style={{ minWidth: '350px' }}>
+                Function name
+              </Title>
+            </Flex>
+            <Card style={{ minWidth: '500px', minHeight: '220px' }}>
+              <Flex flexDirection="row" className="mt-4">
+                <CiTimer />
                 <Text>
                   <Bold>Avg. duration</Bold>
                 </Text>
@@ -44,19 +49,25 @@ const functionrow = () => {
               <BarList data={data} className="mt-1" />
             </Card>
             <Flex flexDirection="col">
-              <Card decoration="left" decorationColor="gray">
+              <Card style={{ minWidth: '500px', minHeight: '220px' }}>
                 <Metric>20</Metric>
                 <Text>cold calls /week</Text>
-              </Card>
-              <Card decoration="left" decorationColor="gray">
                 <Metric>120ms</Metric>
                 <Text>init time</Text>
               </Card>
             </Flex>
+            <Flex flexDirection="col" style={{ minHeight: '220px' }}>
+              <Card>
+                <Button size="sm">Warm the data</Button>
+              </Card>
+              <Card>
+                <Button size="sm">Read more</Button>
+              </Card>
+            </Flex>
           </Flex>
         </Card>
-      </div>
-    </Flex>
+      </Flex>
+    </div>
   );
 };
 
