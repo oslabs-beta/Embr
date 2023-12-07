2fb809054974c8c9026fb337e7f24d24a4f8615d
Davidasahi <maple11303@gmail.com>
2023-12-07 11:38:39 -0500

    styling

--- primary-app/src/app/components/functionrow.tsx
+++ primary-app/src/app/components/functionrow.tsx
@@ -3,8 +3,6 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { CiTimer } from 'react-icons/ci';
-
 import {
   Flex,
   Bold,
@@ -29,49 +27,69 @@ const data = [
 ];
 
 const warmFunction = () => {
-  fetch('https://k2j68xsjnc.execute-api.us-east-2.amazonaws.com/default/thumbnail-creator', {
-    method: "GET",
-    mode: "cors",
-  })
+  fetch(
+    'https://k2j68xsjnc.execute-api.us-east-2.amazonaws.com/default/thumbnail-creator',
+    {
+      method: 'GET',
+      mode: 'cors',
+    }
+  )
     .then((response) => response.json())
     .then((data) => console.log(data))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 };
 
 const functionrow = () => {
   return (
     <Flex flexDirection="col">
       <div className="flex flex-row items-center">
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          <Card decoration="bottom" decorationColor='gray'>
-            <Flex>
-              <Card style={{ minWidth: '50px' }}>
-                <Title>Invocations</Title>
-              </Card>
-              <Card style={{ minWidth: '350px' }} className="max-w-lg">
-                <Flex className="mt-4">
-                  <Text>
-                    <Bold>Avg. duration</Bold>
-                  </Text>
-                </Flex>
-                <BarList data={data} className="mt-1" />
-              </Card>
-              <Flex flexDirection="col">
-                <Card decoration="left" decorationColor="gray">
-                  <Metric>20</Metric>
-                  <Text>cold calls /week</Text>
-                </Card>
-                <Card decoration="left" decorationColor="gray">
-                  <Metric>120ms</Metric>
-                  <Text>init time</Text>
-                </Card>
+        <Card
+          style={{
+            minWidth: '75rem',
+            borderRadius: '15px',
+            border: '2px solid grey',
+          }}
+          // decoration="bottom"
+          decorationColor="gray"
+        >
+          <Flex>
+            <Card className="max-w-xs">
+              <Title>Invocations</Title>
+            </Card>
+            <Card className="max-w-xs">
+              <Flex className="mt-4">
+                <Text>
+                  <Bold>Avg. duration</Bold>
+                </Text>
               </Flex>
+              <BarList data={data} className="mt-1" />
+            </Card>
+            <Flex flexDirection="col">
+              <Card
+                decoration="left"
+                decorationColor="gray"
+                className="max-w-xs"
+              >
+                <Metric>20</Metric>
+                <Text>cold calls /week</Text>
+              </Card>
+              <Card
+                decoration="left"
+                decorationColor="gray"
+                className="max-w-xs"
+              >
+                <Metric>120ms</Metric>
+                <Text>init time</Text>
+              </Card>
             </Flex>
-          </Card>
-        </div>
-        <div onClick={() => warmFunction()} className="cursor-pointer text-center align-middle font-mono bg-violet-600 w-32 p-2 rounded hover:bg-blue-600">
-          Warm
-        </div>
+            <div
+              onClick={() => warmFunction()}
+              className="cursor-pointer text-center align-middle font-mono bg-violet-600 w-72 p-2 rounded hover:bg-blue-600"
+            >
+              Warm
+            </div>
+          </Flex>
+        </Card>
       </div>
     </Flex>
   );
