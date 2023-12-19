16ecc28a998ee1c1e8aee3fa16a3cd0b15a5d4f7
Jade Majed BuGhanem <majed.bg@gmail.com>
2023-12-19 16:23:51 -0500

    Update next.config.js
    Env had AWS credentials ~ removed them for security reasons

--- primary-app/next.config.js
+++ primary-app/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    accessKeyId: 'AKIAVBUUFLGEMVEXMBI3',
-    secretAccessKey: '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',
-    region: 'us-east-2',
   },
 }
 
