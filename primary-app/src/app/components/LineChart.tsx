30814b8c564115b905fe92b3cf4259e49933cdf6
Jade BuGhanem <majed.bg@gmail.com>
2023-12-19 15:45:25 -0500

    Added Titan's edits, to be merged into development. Working off development branch. Additions are: LineChart, Accordions component and few calculations (to be exported). > > Co-authored-by: Titan Tran titanrtran@gmail.com

---
+++ primary-app/src/app/components/LineChart.tsx
@@ -0,0 +1,39 @@
+'use client'
+import { Card, Metric, Text, Divider, AreaChart } from '@tremor/react';
+
+  const LineChart = ({ oneData }) => {
+
+    const valueFormatter = (number: number | bigint) => `${Intl.NumberFormat('us').format(number).toString()} ms`;
+
+    const formatter = (info) => {
+      let result = [];
+      for (let i = 0; i < info.warmInvocationsDuration.length; i++) {
+        let entry = {
+          Timestamp: info.timestamp.slice(info.timestamp.indexOf(' ') + 1, info.timestamp.indexOf('.') - 3),
+          RunTime: info.warmInvocationsDuration[i],
+        };
+        result.push(entry);
+      }
+      return result;
+    };
+    const data = formatter(oneData);
+    
+    return (
+      <Card className='mx-auto'>
+        <Text>titans-lambda-logs</Text>
+        <Metric>Initialization Runtime: {oneData.initDuration}ms </Metric>
+        <AreaChart
+          className='mt-8 h-44'
+          data={data}
+          categories={['RunTime']}
+          index='Timestamp'
+          colors={['indigo']}
+          valueFormatter={valueFormatter}
+          showYAxis={true}
+          showLegend={true}
+        />
+      </Card>
+    );
+  };
+
+export default LineChart;
